1e01efd93e3ea8a55cee491212d3c05995f14c48
Alejandro Sánchez <40835888+alejandroszg@users.noreply.github.com>
2024-12-01 23:19:41 +0000

    lesson 1: creating 'book' class

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,41 @@
 // Write your code here
+class Book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if( this.numCopies === 0 ) {
+      return 'Out of stock'
+    } else if ( this.numCopies < 10 ) {
+      return 'low stock'
+    } else if( this.numCopies > 10 ) {
+      return 'in stock'
+    }
+  }
+
+  sell(numCopiesSold = 1) {
+    return this.numCopies -= numCopiesSold
+  }
+
+  restock(numCopiesStocked = 5) {
+    return this.numCopies += numCopiesStocked
+  }
+}
+
+const atomicHabits = new Book("Atomic Habits", "James Clear", 9783442178582, 50)
+
+console.log(atomicHabits.getAvailability());
+
+atomicHabits.restock(94);
+console.log(atomicHabits.getAvailability());
+
+atomicHabits.sell(144);
+console.log(atomicHabits.getAvailability());

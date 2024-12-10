a3ae9e769f9a8d4460ea447b5e7a3b431e46bcc1
Alejandro Sánchez <40835888+alejandroszg@users.noreply.github.com>
2024-12-10 15:40:01 +0000

    feat: lessons 2 and 3 done!
    fix: changes on my vscode settings

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,20 @@
 // Write your code here
+
+class Movie {
+  constructor(title, genre, director, releaseYear, rating) {
+    this.title = title,
+    this.genre = genre,
+    this.director = director,
+    this.releaseYear = releaseYear,
+    this.rating = rating
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+  }
+}
+
+const inglouriousBasterds = new Movie('Bastardos sin Gloria', 'Dark comedy', 'Quentin Tarantino', 2009, 8.4);
+
+console.log(inglouriousBasterds)
+console.log(inglouriousBasterds.getOverview())

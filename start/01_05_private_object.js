7598000828631353c23f3aecfffb0653384ec87f
Alejandro Sánchez <40835888+alejandroszg@users.noreply.github.com>
2024-12-10 19:36:06 +0000

    feat: lesson 5 Symbol

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,13 @@
 // Write your code here
+const username = Symbol("user");
+const password = Symbol("pass");
+
+const user = {
+  [username]: "alejandro",
+  [password]: 123456,
+  age: 20
+}
+
+console.log(user)
+console.log(user.username)
+console.log(user.password)

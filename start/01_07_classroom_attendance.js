45d3c4e153b5219bd1213cd64a9e092851eeecf5
Alejandro Sánchez <40835888+alejandroszg@users.noreply.github.com>
2024-12-18 01:01:01 +0000

    feat: lesson 7, destructuring

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
 // Write your code here
+function getStudents(classRoom) {
+  let { hasTeachingAssistant, classList } = classRoom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+
+}
+
+console.log(getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+}));
